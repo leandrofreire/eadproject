9536731991b225edde57d0266e84076cbcf4ced3
Leandro <rafaelleandrofreire@gmail.com>
2020-03-29 20:37:51 -0300

    templates

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name='core'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='inicio'),
     path('contato', views.contato, name='contato'),
 ]
