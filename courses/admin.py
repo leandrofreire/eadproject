552ef281490b367e3e8d9aaad0388a59029476c6
Leandro <rafaelleandrofreire@gmail.com>
2020-03-31 22:49:12 -0300

    Customizando o ADMIN

--- courses/admin.py
+++ courses/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from .models import Course
 
-admin.site.register(Course)
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'start_date', 'created_at']
+    search_fields = ['name', 'slug']
+
+
+admin.site.register(Course, CourseAdmin)
