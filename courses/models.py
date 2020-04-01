e9d65c5b7eeed65578efdfc00157c269b4c672a3
Leandro <rafaelleandrofreire@gmail.com>
2020-03-31 22:33:26 -0300

    Admin

--- courses/models.py
+++ courses/models.py
@@ -20,3 +20,6 @@ class Course(models.Model):
     created_at = models.DateTimeField('Criado em', auto_now_add=True)
     updated_at = models.DateTimeField('Atualizado em', auto_now=True)
     objects = CourseManager()
+
+    def __str__(self):
+        return self.name
