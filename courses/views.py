c861a279070368def2c9e8768dcecd56e550c5d4
Leandro <rafaelleandrofreire@gmail.com>
2020-04-04 23:12:53 -0300

    Formulario de duvidas

--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import Course
+from .forms import ContactCourse
 
 
 # Create your views here.
@@ -16,8 +17,15 @@ def index(request):
 
 def detalhe(request, course_slug):
     course = get_object_or_404(Course, slug=course_slug)
-    context = {
-        'course': course
-    }
+    context = {}
+    if request.method == 'POST':
+        form = ContactCourse(request.POST)
+        if form.is_valid():
+            context['is_valid'] = True
+            form = ContactCourse()
+    else:
+        form = ContactCourse()
+    context['form'] = form
+    context['course'] = course
     template_name = 'courses/detalhe.html'
     return render(request, template_name, context)
