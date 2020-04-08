59fe114a5718dcbce28bc733f637b322477d0cea
Leandro <rafaelleandrofreire@gmail.com>
2020-04-07 21:41:09 -0300

    Logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 from . import views
 app_name = 'accounts'
 
 urlpatterns = [
     path('entrar/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('sair/', LogoutView.as_view(next_page='core:inicio'), name='login'),
     path('cadastrar/', views.register, name='register'),
 ]
