b8552077504534a71f63b8382641376adcdcab96
Leandro <rafaelleandrofreire@gmail.com>
2020-04-10 15:35:22 -0300

    Criando um dashboard para o usuário

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('entrar/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('sair/', LogoutView.as_view(next_page='core:inicio'), name='login'),
     path('cadastrar/', views.register, name='register'),
+    path('', views.dashboard, name='dashboard')
 ]
