08779cfa1d6f2cc34710cae246df2621d5180625
Leandro <rafaelleandrofreire@gmail.com>
2020-04-14 22:49:36 -0300

    Comentparios em anuncios

--- courses/urls.py
+++ courses/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
 
-
 from . import views
 
-app_name='courses'
+app_name = 'courses'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    #path('<int:course_id>/', views.detalhe, name='detalhe'),
+    # path('<int:course_id>/', views.detalhe, name='detalhe'),
     path('<slug:course_slug>/', views.detalhe, name='detalhe'),
     path('<slug:slug>/inscricao/', views.enrollment, name='enrollment'),
     path('<slug:slug>/anuncios/', views.announcements, name='announcements'),
+    path('<slug:slug>/anuncios/<int:pk>/', views.show_announcement, name='show_announcement'),
     path('<slug:slug>/cancelar-inscricao/', views.undo_enrollment, name='undo_enrollment')
 
 ]
--- courses/views.py
+++ courses/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 
-from .models import Course, Enrollment
+from .models import Course, Enrollment, Announcement
 from .forms import ContactCourse
 
 
@@ -61,9 +61,9 @@ def undo_enrollment(request, slug):
     template_name = 'courses/undo_enrollment.html'
     context = {
         'enrollment': enrollment,
-        'course':course
+        'course': course
     }
-    return  render(request, template_name, context)
+    return render(request, template_name, context)
 
 
 @login_required
@@ -80,5 +80,24 @@ def announcements(request, slug):
     context = {
         'course': course,
         'announcements': course.announcements.all()
+    }
+    return render(request, template_name, context)
+
+
+@login_required
+def show_announcement(request, slug, pk):
+    course = get_object_or_404(Course, slug=slug)
+    if not request.user.is_staff:
+        enrollment = get_object_or_404(
+            Enrollment, user=request.user, course=course
+        )
+        if not enrollment.is_approved():
+            messages.error(request, 'A sua inscrição está pendente')
+            return redirect('accounts:dashboard')
+    template_name = 'courses/show_announcement.html'
+    announcement = get_object_or_404(course.announcements.all(), pk=pk)
+    context = {
+        'course': course,
+        'announcement': announcement
     }
     return render(request, template_name, context)
