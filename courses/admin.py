1e15996bae28e8c29ab9c749ff6cbb51ee2fb922
Leandro <rafaelleandrofreire@gmail.com>
2020-04-18 16:35:10 -0300

    Criando o admin das aulas

--- courses/admin.py
+++ courses/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Course, Enrollment, Announcement, Comment
+from .models import Course, Enrollment, Announcement, Comment, Lesson, Material
 
 
 class CourseAdmin(admin.ModelAdmin):
@@ -10,5 +10,19 @@ class CourseAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ('name',)}
 
 
+class MaterialInlineAdmin(admin.StackedInline):
+    model = Material
+
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['name', 'number', 'course', 'release_date']
+    search_fields = ['name', 'description']
+    list_filter = ['created_at']
+    inlines = [
+        MaterialInlineAdmin
+    ]
+
+
 admin.site.register(Course, CourseAdmin)
-admin.site.register([Enrollment, Announcement, Comment])
+admin.site.register([Enrollment, Announcement, Comment, Material])
+admin.site.register(Lesson, LessonAdmin)
